af61bbb01531d1bde15149186470c226fd80f27f
Lucas Calebe <lucascalebe07@hotmail.com>
2020-08-27 00:17:49 -0300

    criar tabelas pedido e item-pedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,45 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ItemPedido {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Integer quantidade;
+	
+	@Column(nullable = false)
+	private BigDecimal precoUnitario;
+	
+	@Column(nullable = false)
+	private BigDecimal precoTotal;
+	
+	private String observacao;
+	
+	@ManyToOne
+	@JoinColumn(name = "pedido_id", nullable = false)
+	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,70 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pedido {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private BigDecimal subtotal;
+	
+	@Column(nullable = false)
+	private BigDecimal taxaFrete;
+	
+	@Column(nullable = false)
+	private BigDecimal valorTotal;
+	
+	@Column(nullable = false)
+	@CreationTimestamp
+	private LocalDateTime dataCriacao;
+	
+	private LocalDateTime dataConfirmacao;
+	private LocalDateTime dataCancelamento;
+	private LocalDateTime dataEntrega;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Usuario cliente;
+	
+	@Embedded
+	private Endereco enderecoEntrega;
+	
+	private StatusPedido status;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private FormaPagamento formaPagamento;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
+	
+}
