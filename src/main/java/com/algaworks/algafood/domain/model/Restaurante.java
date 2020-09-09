d168d5eb1513c57808e3d20a688cdad399a6d6f9
Lucas Calebe <lucascalebe07@hotmail.com>
2020-09-09 14:55:47 -0300

    11.2 @JsonIgnoreProperties na propriedade cozinha(nome) em Restaurante

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -29,6 +29,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -55,7 +56,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
-	
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	@Valid
 	@ConvertGroup(from = Default.class,to = Groups.CozinhaId.class)
 	@NotNull
