b8366e748da3de45e115d171ee80e487bbd4a3b8
Lucas Calebe <lucascalebe07@hotmail.com>
2020-09-14 08:40:24 -0300

    12.20 Resolvendo n+1 para usar so um select na listagem de Pedidos e retornando um PedidoResumoModel na listagem

---
+++ src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.algaworks.algafood.domain.model.StatusPedido;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PedidoResumoModel {
+
+	private Long id;
+	private BigDecimal subtotal;
+	private BigDecimal taxaFrete;
+	private BigDecimal valorTotal;
+	private StatusPedido status;
+	private OffsetDateTime dataCriacao;
+	private RestauranteResumoModel restaurante;
+	private UsuarioModel cliente;
+}
