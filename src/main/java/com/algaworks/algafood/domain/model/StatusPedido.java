d1f5f927b28065acfb251f6ed5e69ffcff07f78e
Lucas Calebe <lucascalebe07@hotmail.com>
2020-09-14 16:22:31 -0300

    12.23 implementando endpoints de transição de status

--- src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
@@ -2,8 +2,18 @@ package com.algaworks.algafood.domain.model;
 
 public enum StatusPedido {
 	
-	CRIADO,
-	CONFIRMADO,
-	ENTREGUE,
-	CANCELADO;
+	CRIADO("Criado"),
+	CONFIRMADO("Confirmado"),
+	ENTREGUE("Entregue"),
+	CANCELADO("Cancelado");
+	
+	private String descricao;
+	
+	private StatusPedido(String descricao) {
+		this.descricao = descricao; 
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
 }
