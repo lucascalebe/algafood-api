d704dfe03cddd87f78150136d3359e2eff91c28b
Lucas Calebe <lucascalebe07@hotmail.com>
2020-09-17 12:42:47 -0300

    13.20 parte 2: Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -22,28 +22,26 @@ public class EstatisticasController {
 
 	@Autowired
 	private VendaQueryService vendaQueryService;
-	
+
 	@Autowired
-	private  VendaReportService vendaReportService;
-	
+	private VendaReportService vendaReportService;
+
 	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
-			@RequestParam(required =  false, defaultValue = "+00:00") String timeOffset) {
-		return vendaQueryService.consultarVendasDiarias(filtro,timeOffset);
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
 	}
-	
+
 	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
-			@RequestParam(required =  false, defaultValue = "+00:00") String timeOffset) {
-		
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+
 		byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
-		
+
 		var headers = new HttpHeaders();
 		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
-		
-		return ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_PDF)
-				.headers(headers)
-				.body(bytesPdf);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).headers(headers).body(bytesPdf);
 	}
+
 }
