9f5f7c4f7e90d3155611d5ca4f860ad42d1a997b
Lucas Calebe <lucascalebe07@hotmail.com>
2020-09-23 15:18:17 -0300

    14.15. Desafio: implementando endpoint de exclusão de foto de produto

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -50,6 +50,16 @@ public class CatalogoFotoProdutoService {
 		return foto;
 	}
 	
+	@Transactional
+	public void remover(Long restauranteId, Long produtoId) {
+		FotoProduto foto = buscarOuFalhar(restauranteId, produtoId);
+		
+		produtoRepository.delete(foto);
+		produtoRepository.flush();
+		
+		fotoStorage.remover(foto.getNomeArquivo());
+	}
+	
 	public FotoProduto buscarOuFalhar(Long restauranteId, Long produtoId) {
 		return produtoRepository.findFotoById(restauranteId, produtoId)
 		.orElseThrow(() -> new FotoProdutoNaoEncontradaException(restauranteId,produtoId));
