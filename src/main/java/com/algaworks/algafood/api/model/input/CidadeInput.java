dd53eb608fdf6cdb38f2b5ab9c3ae322b05e0899
Lucas Calebe <lucascalebe07@hotmail.com>
2020-10-21 17:49:50 -0300

    18.10. Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/model/input/CidadeInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/CidadeInput.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class CidadeInput {
 
+	@ApiModelProperty(example = "Brasília")
 	@NotBlank
 	private String nome;
 	
--- src/main/java/com/algaworks/algafood/api/model/input/EstadoIdInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/EstadoIdInput.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.model.input;
 
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +11,6 @@ import lombok.Setter;
 public class EstadoIdInput {
 
 	@NotNull
+	@ApiModelProperty(example = "1")
 	private Long id;
 }
