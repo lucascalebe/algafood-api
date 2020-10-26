b758e7dc45a233413df1774f4558f53f9c038252
Lucas Calebe <lucascalebe07@hotmail.com>
2020-10-26 15:10:58 -0300

    18.12. Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,8 +1,12 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,8 +14,10 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -31,12 +37,28 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 					.paths(PathSelectors.any())
 					.build()
+				.useDefaultResponseMessages(false)	
+				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessage())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));
 	}
 	
+	private List<ResponseMessage> globalGetResponseMessage() {
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+					.code(500)
+					.message("Erro interno do servidor")
+					.build(),
+					
+				new ResponseMessageBuilder()
+					.code(406)
+					.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+					.build()
+				);
+	}
+	
 	// informações na pagina HTML do swagger UI
-	public ApiInfo apiInfo() {
+	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("Algafood API")
 				.description("API aberta para clientes e restaurantes")
