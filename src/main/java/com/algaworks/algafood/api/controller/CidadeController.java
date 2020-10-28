397e700055f9ff8aeeaba9957c76372b15a31fc5
Lucas Calebe <lucascalebe07@hotmail.com>
2020-10-28 12:28:43 -0300

    18.16. Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
 import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
+import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.input.CidadeInput;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
@@ -29,6 +30,8 @@ import com.algaworks.algafood.domain.service.CadastroCidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Cidades")
 @RestController
@@ -54,6 +57,10 @@ public class CidadeController {
 	}
 
 	@ApiOperation("Busca uma cidade por Id")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+	})
 	@GetMapping("/{cidadeId}")
 	public CidadeModel buscar(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId) {
 		return cidadeModelAssembler.toModel(cadastroCidade.buscarOuFalhar(cidadeId));
@@ -76,6 +83,10 @@ public class CidadeController {
 	}
 	
 	@ApiOperation("Atualiza uma cidade por Id")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Cidade Atualizada"),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+	})
 	@PutMapping("/{cidadeId}")
 	public CidadeModel atualizar(@ApiParam(value = "ID de uma cidade",example = "1") @PathVariable Long cidadeId,
 			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") 
@@ -94,6 +105,10 @@ public class CidadeController {
 	}
 	
 	@ApiOperation("Exclui uma cidade por Id")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Cidade excluída"),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+	})
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId) {
