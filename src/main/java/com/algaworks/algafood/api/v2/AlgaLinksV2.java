4e02927e8a9b239bc20c6a3921de49c88db484bc
Lucas Calebe <lucascalebe07@hotmail.com>
2021-11-20 19:09:26 -0300

    20.15. adicionando os recursos de cozinhas na v2 da API

--- src/main/java/com/algaworks/algafood/api/v2/AlgaLinksV2.java
+++ src/main/java/com/algaworks/algafood/api/v2/AlgaLinksV2.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.v2;
 
 import com.algaworks.algafood.api.v2.controller.CidadeControllerV2;
+import com.algaworks.algafood.api.v2.controller.CozinhaControllerV2;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -15,5 +16,13 @@ public class AlgaLinksV2 {
 
 	public Link linkToCidades() {
 		return linkToCidades(IanaLinkRelations.SELF.value());
+	}
+
+	public Link linkToCozinhas(String rel) {
+		return WebMvcLinkBuilder.linkTo(CozinhaControllerV2.class).withRel(rel);
+	}
+
+	public Link linkToCozinhas() {
+		return linkToCozinhas(IanaLinkRelations.SELF.value());
 	}
 }
---
+++ src/main/java/com/algaworks/algafood/api/v2/controller/CozinhaControllerV2.java
@@ -0,0 +1,79 @@
+package com.algaworks.algafood.api.v2.controller;
+
+import com.algaworks.algafood.api.v1.model.input.CozinhaInput;
+import com.algaworks.algafood.api.v1.openapi.controller.CozinhaControllerOpenApi;
+import com.algaworks.algafood.api.v2.assembler.CozinhaInputDisassemblerV2;
+import com.algaworks.algafood.api.v2.assembler.CozinhaModelAssemblerV2;
+import com.algaworks.algafood.api.v2.model.CozinhaModelV2;
+import com.algaworks.algafood.api.v2.model.input.CozinhaInputV2;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/v2/cozinhas",produces = MediaType.APPLICATION_JSON_VALUE)
+public class CozinhaControllerV2 {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
+	@Autowired
+	private CozinhaModelAssemblerV2 cozinhaModelAssembler;
+	
+	@Autowired
+	private CozinhaInputDisassemblerV2 cozinhaInputDisassembler;
+	
+	@Autowired
+	private PagedResourcesAssembler<Cozinha> pagedResourceAssembler;
+	
+	@GetMapping
+	public PagedModel<CozinhaModelV2> listar(@PageableDefault(size = 10) Pageable pageable) {
+		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
+
+		PagedModel<CozinhaModelV2> cozinhasPagedModel = pagedResourceAssembler.
+				toModel(cozinhasPage,cozinhaModelAssembler);
+
+		return cozinhasPagedModel;
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public CozinhaModelV2 buscar(@PathVariable Long cozinhaId) {
+		return cozinhaModelAssembler.toModel(cadastroCozinha.buscarOuFalhar(cozinhaId));
+	}	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public CozinhaModelV2 adicionar(@RequestBody @Valid CozinhaInputV2 cozinhaInput) {
+		Cozinha cozinha = cozinhaInputDisassembler.toDomain(cozinhaInput);
+		return cozinhaModelAssembler.toModel(cadastroCozinha.salvar(cozinha));
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public CozinhaModelV2 atualizar(@PathVariable Long cozinhaId, @RequestBody @Valid CozinhaInputV2 cozinhaInput) {
+		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
+
+		cozinhaInputDisassembler.toDomainObject(cozinhaInput, cozinhaAtual);
+
+		return cozinhaModelAssembler.toModel(cadastroCozinha.salvar(cozinhaAtual));
+	}
+	
+	@DeleteMapping("/{cozinhaId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId) {	
+		cadastroCozinha.excluir(cozinhaId);
+	}		
+}
