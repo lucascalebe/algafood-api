d7566328d1bb9d8c5b930d3c3bdeb52a8c3b75ce
Lucas Calebe <lucascalebe07@hotmail.com>
2021-12-12 10:53:33 -0300

    23.20. Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,15 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
-import javax.crypto.spec.SecretKeySpec;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -18,11 +19,32 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
-                .anyRequest().authenticated()
+                    .antMatchers(HttpMethod.POST,"/v1/cozinhas/**").hasAnyAuthority("EDITAR_COZINHAS")
+                    .antMatchers(HttpMethod.PUT,"/v1/cozinhas/**").hasAnyAuthority("EDITAR_COZINHAS")
+                    .antMatchers(HttpMethod.GET,"/v1/cozinhas/**").authenticated()
+                .anyRequest().denyAll()
                     .and()
                     .cors().and()
-                    .oauth2ResourceServer().jwt();
+                    .oauth2ResourceServer()
+                    .jwt()
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter());
     }
 
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter =  new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null) {
+                authorities = Collections.emptyList();
+            }
+
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
+    }
 
 }
