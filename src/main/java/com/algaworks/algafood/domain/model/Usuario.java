73d3d58fa6bfc0f9e291d063036b071eabcc1a74
Lucas Calebe <lucascalebe07@hotmail.com>
2021-12-11 21:07:29 -0300

    23.15. Desafio: refatorando serviços de usuários para usar BCrypt

--- src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -53,7 +53,11 @@ public class Usuario {
 	public boolean senhaNaoCoincideCom(String senha) {
 		return !senhaCoincideCom(senha);
 	}
-	
+
+	public boolean isNovo() {
+		return getId() == null;
+	}
+
 	public void adicionarGrupo(Grupo grupo) {
 		getGrupos().add(grupo);
 	}
--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +21,10 @@ public class CadastroUsuarioService {
 	
 	@Autowired
 	private CadastroGrupoService cadastroGrupo;
-	
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
 		usuarioRepository.detach(usuario);
@@ -31,6 +35,11 @@ public class CadastroUsuarioService {
 			throw new NegocioException(String.format(
 					"Já existe um usuário cadastrado com o email %s",usuario.getEmail()));
 		}
+
+		if (usuario.isNovo()) {
+			usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		}
+
 		return usuarioRepository.save(usuario);
 	}
 	
@@ -38,11 +47,11 @@ public class CadastroUsuarioService {
 	public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
 		Usuario usuario = buscarOuFalhar(usuarioId);
 	
-		if (usuario.senhaNaoCoincideCom(senhaAtual)) {
+		if (!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
 			throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
 		}
 		
-		usuario.setSenha(novaSenha);
+		usuario.setSenha(passwordEncoder.encode(novaSenha));
 		
 	}
 	
